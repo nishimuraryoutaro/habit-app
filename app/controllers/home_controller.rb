5aef402d64b9181c838067af5698144b3548d6a9
ricardo <nishimu75618094@gmail.com>
2025-09-06 20:00:07 +0900

    タイムゾーン設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    return unless user_signed_in?
-
     @final_goal = current_user.goals.order(target_date: :desc).first
   end
 end
