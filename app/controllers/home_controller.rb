c4f9b20ba1428305d82bd89f204638a5d85a5e06
ricardo <nishimu75618094@gmail.com>
2025-09-07 23:36:49 +0900

    focusedgoal
    Signed-off-by: ricardo <nishimu75618094@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,16 +9,19 @@ class HomeController < ApplicationController
     else
       if raw.match?(/\A\d{4}-\d{2}-\d{2}\z/)
         y, m, d = raw.split("-").map(&:to_i)
-        if Date.valid_date?(y, m, d)
-          @selected_date = Date.new(y, m, d)
-        else
-          @selected_date = Date.current
-        end
+          if Date.valid_date?(y, m, d)
+            @selected_date = Date.new(y, m, d)
+          else
+            @selected_date = Date.current
+          end
       else
         @selected_date = Date.current
       end
     end
+    @focused_goal = if params[:goal_id].present?
+    current_user.goals.find_by(id: params[:goal_id])
+    end
+    @final_goal = @focused_goal || current_user.goals.order(:target_date, :created_at).first
 
-    @final_goal = current_user.goals.order(target_date: :desc).first
   end
 end
