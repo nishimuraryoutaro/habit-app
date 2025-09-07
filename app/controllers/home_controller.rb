53a33d2f4d9904bc27a407e88e5dd8d29bbaefac
ricardo <nishimu75618094@gmail.com>
2025-09-07 20:08:46 +0900

    日付処理の修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,21 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    raw = params[:date]
+    # 日付のパース処理
+    raw = params[:date].to_s.strip
 
     if raw.empty?
        @selected_date = Date.current
     else
       if raw.match?(/\A\d{4}-\d{2}-\d{2}\z/)
-        y, m, d = raw.split("-")
+        y, m, d = raw.split("-").map(&:to_i)
         if Date.valid_date?(y, m, d)
           @selected_date = Date.new(y, m, d)
         else
           @selected_date = Date.current
         end
+      else
+        @selected_date = Date.current
       end
     end
 
