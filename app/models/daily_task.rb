63730d401238ffeae07b3d763e127c8f649a1179
ricardo <nishimu75618094@gmail.com>
2025-09-08 20:04:06 +0900

    add-three-task conditions
    Signed-off-by: ricardo <nishimu75618094@gmail.com>

--- app/models/daily_task.rb
+++ app/models/daily_task.rb
@@ -1,4 +1,13 @@
 class DailyTask < ApplicationRecord
   belongs_to :user
   belongs_to :goal
+
+  def max_three_per_day
+    if date.present? && goal_id.present?
+      count = DailyTask.where(user_id: user_id, goal_id: goal_id, date: date).where.not(id: id).count
+       if count >= 3
+        errors.add(:base, "1日に作成できるタスクは3件までです")
+       end
+    end
+  end
 end
