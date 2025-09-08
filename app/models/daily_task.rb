a0f40c65a1d29e08d10693224d67b3415056a641
ricardo <nishimu75618094@gmail.com>
2025-09-08 20:31:13 +0900

    daily-task validates
    Signed-off-by: ricardo <nishimu75618094@gmail.com>

--- app/models/daily_task.rb
+++ app/models/daily_task.rb
@@ -2,6 +2,10 @@ class DailyTask < ApplicationRecord
   belongs_to :user
   belongs_to :goal
 
+  validates :title, presence: true
+  validates :date, presence: true
+  validate :max_three_per_day
+
   def max_three_per_day
     if date.present? && goal_id.present?
       count = DailyTask.where(user_id: user_id, goal_id: goal_id, date: date).where.not(id: id).count
